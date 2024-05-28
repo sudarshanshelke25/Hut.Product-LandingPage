ef7f31141429618ffe497dea776ac43402904ca5
SudarshanShelke <sudarshanshelke@gmail.com>
2024-05-29 00:36:41 +0530

    1st Commit: Create LandingPage for Hut.Product

---
+++ script.js
@@ -0,0 +1,33 @@
+const navSearch = document.getElementById("nav-search");
+
+navSearch.addEventListener("click", (e) => {
+    navSearch.classList.toggle('open');
+});
+
+const scrollRevealOption = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
+};
+
+ScrollReveal().reveal(".header-image img", {
+    ...scrollRevealOption,
+    origin: "right",
+});
+
+ScrollReveal().reveal(".header-content .section-subheader", {
+    ...scrollRevealOption,
+    origin: "left",
+    delay: 500,
+});
+
+ScrollReveal().reveal(".header-content h1", {
+    ...scrollRevealOption,
+    delay: 1000,
+});
+
+ScrollReveal().reveal(".header-content .header-btn", {
+    ...scrollRevealOption,
+    delay: 1500,
+});
+
